1938dca47731ffa504a7ce2f357a5729a00fb49f
John Meissner <johnmeissner@Johns-MacBook-Pro.local>
2021-10-15 08:48:36 +0100

    first commit

---
+++ REST API.js
@@ -0,0 +1,17 @@
+/* 
+With webpages we have requests
+
+REST API -> stands for Representational state transfer
+
+There are 5 types of requests and these are usually in Javascript script object notation(JSON) format.
+
+objects are put in {} to declare them
+
+C - Post request
+R - Get request
+U - Put/patch request (put is overriding all information and patch is updating the info in a certain place)
+D - Delete
+
+
+
+*/
